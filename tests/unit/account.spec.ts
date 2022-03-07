fbcd57c6e5869d39968e2117863b7b6756928555
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-03-07 17:50:33 +0100

    Use mocking framework instead of fake implementation

--- tests/unit/account.spec.ts
+++ tests/unit/account.spec.ts
@@ -2,15 +2,10 @@ import Account from "../../src/account";
 
 describe('Account', () => {
     it('prints only the header when no operation were made', () => {
-        const messages = [];
-        const console = (message) => {
-            messages.push(message)
-        }
+        const console = jest.fn()
         const account = new Account(console)
 
         account.printStatement()
-        expect(messages).toStrictEqual([
-            "Date || Amount || Balance"
-        ])
+        expect(console).toHaveBeenCalledWith("Date || Amount || Balance");
     });
 });
